b8b1a52689c84b3a70485813920281a798f43988
ziedll <112785620+ziedll@users.noreply.github.com>
2026-03-12 14:56:45 +0100

    Ajout formulaire html

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 <?php
+
+?>
+<form action="" method="POST">
+    <input type="text" placeholder="nom">
+    <input type="text" placeholder="age">
+    <select name="" id="" placeholder="role">
+        <option value="Directeur">Directeur</option>
+        <option value="collaborateur"></option>
+    </select>
+</form>
